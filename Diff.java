cb45842cf46a76b17551f7254cc3d56cadcd3fa9
aditya <nagleaditya@sggs.ac.in>
2022-09-05 20:03:24 +0530

    Modified Add file

--- Diff.java
+++ Diff.java
@@ -1,7 +1,7 @@
-class Add {
+class Diff {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        System.out.println("The sum is :"+(a+b));
+        System.out.println("The diff is :"+(a-b));
     }
 }
